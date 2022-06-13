869e7f83d84d1460e98bd921dcf776721fb29e91
gambarov <gambarov.20015960@gmail.com>
2022-06-14 01:18:00 +0500

    add basic react-table impl

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Column } from 'react-table';
+import EquipmentTable, { EquipmentData } from './components/EquipmentTable';
 
 function App() {
+	const headers: Column<EquipmentData>[] = React.useMemo(() => [
+		{
+			Header: "#",
+			accessor: "id"
+		},
+		{
+			Header: "Наименование",
+			accessor: "name"
+		},
+		{
+			Header: "Тип",
+			accessor: "type",
+		}
+	], []);
+
+	const data: EquipmentData[] = React.useMemo(() => [
+		{ id: 1, name: 'Прибор2', type: 'Тип2' },
+	], []);
+
 	return (
-		<div>
-			Hello!
-		</div>
+		<EquipmentTable columns={headers} data={data} />
 	);
 }
 
---
+++ src/components/EquipmentTable.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+import { useTable, Column } from 'react-table'
+
+export interface EquipmentData {
+    id: number;
+    name: string;
+    type: string;
+}
+
+interface Props {
+    columns: Column<EquipmentData>[];
+    data: EquipmentData[];
+}
+
+const EquipmentTable: React.FC<Props> = ({ columns, data }) => {
+    // Use the state and functions returned from useTable to build your UI
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({
+        columns,
+        data,
+    })
+
+    console.log(rows);
+
+    return (
+        <Table {...getTableProps()}>
+            <thead>
+                {headerGroups.map((headerGroup) => {
+                    const {key, ...restHeadGroupProps} = headerGroup.getHeaderGroupProps();
+                    return (<tr {...restHeadGroupProps} key={key}>
+                        {headerGroup.headers.map(column => {
+                            const {key, ...restColumnProps} = column.getHeaderProps();
+                            return (
+                                <th {...restColumnProps} key={key}>{column.render('Header')}</th>
+                            )
+                        })}
+                    </tr>)
+                })}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row) => {
+                    prepareRow(row)
+                    const { key, ...restRowProps } = row.getRowProps();
+                    return (
+                        <tr {...restRowProps} key={key}>
+                            {row.cells.map(cell => {
+                                const { key, ...restCellProps } = cell.getCellProps();
+                                return <td {...restCellProps} key={key}>{cell.render('Cell')}</td>
+                            })}
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </Table>
+    )
+}
+
+export default EquipmentTable;
