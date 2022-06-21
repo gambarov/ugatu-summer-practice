5fab32a72641fccd0dda77020e1f1ecebb46fe39
gambarov <gambarov.20015960@gmail.com>
2022-06-21 15:02:43 +0500

    add main api controllers routing

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,29 @@
 <?php
 
+use App\Http\Controllers\API\V1\AudienceController;
+use App\Http\Controllers\API\V1\EquipmentController;
+use App\Http\Controllers\API\V1\EquipmentSetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
+| These routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/equipment', EquipmentController::class, [
+    'only' => ['index', 'store', 'show', 'update', 'destroy'],
+]);
+
+
+Route::apiResource('/sets', EquipmentSetController::class, [
+    'only' => ['index', 'store', 'show', 'update', 'destroy'],
+]);
+
+Route::apiResource('/audiences', AudienceController::class, [
+    'only' => ['index', 'store', 'show', 'update', 'destroy'],
+]);
