88266832da6020770201eef158729401de40742c
gambarov <gambarov.20015960@gmail.com>
2022-06-22 15:17:30 +0500

    upd api route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\API\V1\AudienceController;
 use App\Http\Controllers\API\V1\EmployeeController;
 use App\Http\Controllers\API\V1\EquipmentController;
 use App\Http\Controllers\API\V1\EquipmentSetController;
+use App\Http\Controllers\API\V1\NoteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -29,5 +30,9 @@ Route::apiResource('/audiences', AudienceController::class, [
 ]);
 
 Route::apiResource('/employees', EmployeeController::class, [
+    'only' => ['index', 'store', 'show', 'update', 'destroy'],
+]);
+
+Route::apiResource('/notes', NoteController::class, [
     'only' => ['index', 'store', 'show', 'update', 'destroy'],
 ]);
