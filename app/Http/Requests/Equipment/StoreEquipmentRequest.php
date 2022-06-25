a14d45157d8f8c84c8184ac160475c97b4d18b58
gambarov <gambarov.20015960@gmail.com>
2022-06-25 22:29:15 +0500

    add equipment form requests

---
+++ app/Http/Requests/Equipment/StoreEquipmentRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Equipment;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreEquipmentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'equipment_type_id' => 'required|integer|exists:App\Models\Equipment\EquipmentType,id',
+            'inventory_id' => 'required|string|max:255',
+            'sets' => 'sometimes|array',
+            'sets.*' => 'sometimes|integer|exists:App\Models\Equipment\Set,id',
+        ];
+    }
+}
